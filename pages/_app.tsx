7a44db8eb4df1426175324aa72bd67c69b89f2fc
djonko <prevostdjonko@yahoo.fr>
2021-10-14 22:24:34 -0400

    list task by using apolloclient and graphql code gen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
-import '../styles/globals.css'
-import {AppProps} from 'next/app'
-import { useApollo } from '../lib/client'
-import { ApolloProvider } from '@apollo/client'
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import { useApollo } from "../lib/client";
+import { ApolloProvider } from "@apollo/client";
+import Layout from "../componens/Layout";
 
-function MyApp({ Component, pageProps }: AppProps ) {
-  const apolloClient = useApollo(pageProps.initialApolloState)
-  return(
-  <ApolloProvider client={apolloClient}>
-    <Component {...pageProps} />
-  </ApolloProvider>
-  ) 
+function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
